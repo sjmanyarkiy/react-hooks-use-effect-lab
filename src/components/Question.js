8b85f4dbacd4e96cfd6f9a17454a180a5095c6dd
commit-panda <sjmanyarkiy@gmail.com>
2025-11-20 14:44:49 +0300

    completed lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1);
+
+      if(timeRemaining <= 10 && timeRemaining >= 1){
+        return null
+      }
+      else{
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+
+    return function cleanup(){
+      clearTimeout(timerId)
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
